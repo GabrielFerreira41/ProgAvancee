1549df5c7817bfc27bc5b57fd72e2ae4fe2c09f4
FerreiraG <gabriel.ferreira@etu.univ-orleans.fr>
2023-10-12 17:12:23 +0200

    [ADD + Update] Ajout pop up CRUD reussite et erreur + gestion des noms des titre de films qui existe deja

--- Catalogue/Films/views.py
+++ Catalogue/Films/views.py
@@ -48,17 +48,21 @@ def addFilm(request):
 
     if request.method == "POST":
         form = FilmForm(request.POST, request.FILES)
-
+        print(form)
         if form.is_valid():
             title = form.cleaned_data['title']
-            try:
-                existing_film = Films.objects.get(title=title)
-                messages.error(request, 'Un film avec ce nom existe déjà.')
-                return render(request, 'addFilm.html', {'form': form})
-            except ObjectDoesNotExist:
-                newFilm = form.save()
-                messages.success(request, 'Nouveau film ' + newFilm.title)
-                return render(request, template_name='film.html', context={'film': newFilm})
+            normalized_title = title.lower()
+            
+            existing_film = Films.objects.filter(title__iexact=normalized_title).first()
+            
+            if existing_film:
+                errorAddMessage = 'Un film avec ce nom existe déjà.' # Message pour le pop up d'erreur lorsque l'utilisateur choissi un titre de film deja existant
+                return render(request, 'addFilm.html', {'form': form,'errorAddMessage':errorAddMessage}) # on oubli pas d'ajouter le message d'erreur
+            else:
+                new_film = form.save()
+                successAddMessage = "Ajout du film '"+ new_film.title +"' reussi" # Message pour le pop up de reussite d'ajout d'un film en BD
+                return render(request, template_name='film.html', context={'film': new_film,'successAddMessage':successAddMessage}) # on oubli pas d'ajouter le message de reussite
+
 
     context = {'form': form}
     return render(request, 'addFilm.html', context)
@@ -80,7 +84,9 @@ def deleteFilm(request,id):
     film = Films.objects.get(id=id)
     film.delete()
     films = Films.objects.all()
-    return render(request,template_name='films.html',context={'films':films})
+    successfulDeleteMessage = "Suppression du film '" +film.title + "' reussi" #message pop up de reussite de suppression du film
+    print(successfulDeleteMessage)
+    return render(request,template_name='films.html',context={'films':films,'successfulDeleteMessage':successfulDeleteMessage})
 
 
 def updateFilm(request, id):
@@ -95,21 +101,20 @@ def updateFilm(request, id):
         if form.is_valid():
             new_title = form.cleaned_data['title']
             if new_title != filmTitle:
-                try:
-                    existing_film = Films.objects.get(title=new_title)
-                    messages.error(request, 'Un film avec ce nom existe déjà.')
-                    return render(request, 'film.html', {'film': film, 'update': True, 'form': form})
-                except Films.DoesNotExist:
-                    pass
+                lowerNewTitle = new_title.lower()
+                existing_film = Films.objects.filter(title__iexact=lowerNewTitle).first()
+                if existing_film:
+                    errorUpdateMessage = 'Un film avec ce nom existe déjà.' #Message pour le pop d'erreur lorsque l'utilisateur rentre en nom de film déja existant
+                    return render(request, 'film.html', {'form': form,'film': film,'update': True,'errorUpdateMessage':errorUpdateMessage})
 
             form.save()
-
             new_image = request.FILES.get('imageName', None)
             if new_image:
                 film.imageName = new_image
                 film.save()
             film = Films.objects.get(id=id)
-            return render(request, template_name='film.html', context={'film': film, 'update': False})
+            sucessUpdateMessage = 'Mise à jour reussi' # Message pour le pop up de reussite de modification d'un film
+            return render(request, template_name='film.html', context={'film': film, 'update': False,'sucessUpdateMessage':sucessUpdateMessage})
     else:
         form = FilmForm(instance=film)
     return render(request, template_name='film.html', context={'film': film, 'update': True, 'form': form})
